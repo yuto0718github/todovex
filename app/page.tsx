db858360192b12f0af75eb83acc68856ce3e952b
Yuto <yuto.y.0718@gmail.com>
2024-10-31 11:48:54 +0900

    install shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@
 
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <>
-    <h1>ToDoVex</h1>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    <h1>ToDoVex</h1>
     </main>
     </>
   );
