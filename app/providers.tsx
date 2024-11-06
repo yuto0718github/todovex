ebdada207401ca052e1e1828473992fc04513524
YutosCodes <yuto.y.0718@gmail.com>
2024-11-06 10:10:49 +0900

    atuh set up

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { boolean } from "convex-helpers/validators";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { ReactNode, useMemo } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function useAuth() {
+  const {data: session, update} = useSession();
+
+  return useMemo(() => ({
+    isLoading: false,
+    isAuthenticated: session !== null,
+    fetchAccessToken: async({
+      forceRefreshToken
+    }: {forceRefreshToken: boolean}) => {
+      if(forceRefreshToken) {
+        const session = await update()
+        return session?.convexToken ?? null;
+      }
+    }
+  })
+  [JSON.stringify(session?.user)]
+)
+}
 
 export function Providers({ 
   children, 
@@ -16,7 +36,7 @@ export function Providers({
  }) {
   return (
   <SessionProvider session={session}>
-    <ConvexProvider client={convex}>{children}
+    <ConvexProvider client={convex} useAuth={useAuth}>{children}
       </ConvexProvider>
   </SessionProvider>
   );
--- auth.ts
+++ auth.ts
@@ -1,6 +1,20 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { ConvexAdapter } from "./app/ConvexAdapter"
  
+if (process.env.CONVEX_AUTH_PRIVATE_KEY === undefined) {
+  throw new Error('Missing CONVEX_AUTH_PRIVATE_KEY')
+}
+
+if (process.env.JWKS === undefined) {
+  throw new Error('Missing JWKS')
+}
+
+if (process.env.NEXT_PUBLIC_CONVEX_URL === undefined) {
+  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL')
+}
+
+const CONVEX_SITE_URL= process.env.NEXT_PUBLIC_CONVEX_URL!.replace(/.cloud$/, '.site')
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
@@ -9,4 +23,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         authorization: {params: {prompt: "consent"}},
     }),
   ],
+  adapter: ConvexAdapter,
+  callbacks: {
+    async session({session}) {
+      const privateKey = await importPKCS8(process.env.CONVEX_AUTH.PRIVATE_KEY!, 'RS256');
+
+          const convexToken = await new SignJWT({
+            sub: session.userId
+          }).setProtectedHeader({ alg: "RS256"})
+          .setIssuedAt()
+          .setIssuer(CONVEX_SITE_URL)
+          .setAudience('convex')
+          .setExpirationTime('1hr')
+          .sign(privateKey);
+        return {...session, convexToken}
+    }
+  }
 })
